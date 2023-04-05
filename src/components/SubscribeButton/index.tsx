d02c073bb825b9d3ea44a295f1c643c6a57b3d8b
pedro-cal <pedroluna2@hotmail.com>
2023-04-04 22:35:08 -0300

    feature: add subscribe screen

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,10 +8,34 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({}:SubscribeButtonProps) {
+   const session = useSession();
+
+   async function handleSubscribe() {
+      if(!session) {
+         signIn('github');
+         return;
+      }
+
+      // criação da checkout session
+      try {
+         const response = await api.post('/subscribe');
+
+         const { sessionId } = response.data;
+
+         const stripe = await getStripeJs();
+
+         await stripe.redirectToCheckout({ sessionId });
+      } catch (err) {
+         alert(err.message);
+         console.error(err);
+      }
+   }
+   
    return (
       <button
          type="button"
          className={styles.subscribeButton}
+         onClick={handleSubscribe}
       >
          Subscribe Now
       </button>
